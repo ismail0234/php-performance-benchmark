516434a74a3199970198b5941e964ec3b2a8cd8a
İsmail <ismaiil_0234@hotmail.com>
2019-11-25 13:01:48 +0300

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `members` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+ `number` int(11) NOT NULL,
+ `gb` int(11) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
