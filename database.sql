86737193de925837438b6b06b95c267ccd13b7be
İsmail <ismaiil_0234@hotmail.com>
2019-11-25 13:34:06 +0300

    Delete database.sql

--- database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `members` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
- `number` int(11) NOT NULL,
- `gb` int(11) NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
