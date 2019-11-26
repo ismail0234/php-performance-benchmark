4a26cbca67b96ca2bd8f239b2cca1ba014743aec
ismail0234 <ismaiil_0234@hotmail.com>
2019-11-26 17:45:43 +0300

    index dosyası router mekanizmasına entegre edildi

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php 
 
-include "benchmark.php";
+define('ROOT_PATH'       , dirname(__FILE__) . DIRECTORY_SEPARATOR );
+define('APP_PATH'        , ROOT_PATH . 'application' . DIRECTORY_SEPARATOR );
+define('CORE_PATH'       , APP_PATH  . 'core'        . DIRECTORY_SEPARATOR );
+define('CONFIG_PATH'     , APP_PATH  . 'config'      . DIRECTORY_SEPARATOR );
+define('CONTROLLER_PATH' , APP_PATH  . 'controller'  . DIRECTORY_SEPARATOR );
+define('LIB_PATH' 		 , APP_PATH  . 'lib' 		 . DIRECTORY_SEPARATOR );
+define('LANG_PATH' 		 , APP_PATH  . 'language'    . DIRECTORY_SEPARATOR );
+define('VIEW_PATH' 		 , APP_PATH  . 'view' 		 . DIRECTORY_SEPARATOR );
 
-$performance = new DBPerformance('dbname', 'localhost', 'dbusername', 'dbuserpassword');
-
-$performance->insertBenchmark(5000);
-$performance->selectBenchmark(5000);
-$performance->updateBenchmark(5000);
-
-$performance->drawBenchmark();
+/*
+ * LAMBDA ÇEKİRDEK DOSYASI
+ */
+require APP_PATH . "core/core.php";
