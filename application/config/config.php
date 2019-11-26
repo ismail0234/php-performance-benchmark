6b04bbe00aacce6a4c283c82ab8e448f485767d6
ismail0234 <ismaiil_0234@hotmail.com>
2019-11-26 17:46:16 +0300

    Config dosyası eklendi.

---
+++ application/config/config.php
@@ -0,0 +1,46 @@
+<?php 
+
+/**
+ *
+ * SYSTEM Version
+ *
+ * @author Ismail Satilmis <ismaiil_0234@hotmail.com>
+ *
+ */
+define('SYSTEM_VERSION', 'v1.0.0');
+
+/**
+ *
+ * Database Host Address
+ *
+ * @author Ismail Satilmis <ismaiil_0234@hotmail.com>
+ *
+ */
+define('DB_HOST', 'localhost');
+
+/**
+ *
+ * Database UserName
+ *
+ * @author Ismail Satilmis <ismaiil_0234@hotmail.com>
+ *
+ */
+define('DB_USERNAME', 'root');
+
+/**
+ *
+ * Database Password
+ *
+ * @author Ismail Satilmis <ismaiil_0234@hotmail.com>
+ *
+ */
+define('DB_PASSWORD', '');
+
+/**
+ *
+ * Database name
+ *
+ * @author Ismail Satilmis <ismaiil_0234@hotmail.com>
+ *
+ */
+define('DB_NAME', 'test');
